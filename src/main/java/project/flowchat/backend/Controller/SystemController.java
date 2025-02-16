8251da153203f5220454929f6495edb963c29561
Edwin Lam <edwinlamtk@gmail.com>
2025-02-16 16:20:38 +0800

    Deploy 2 APIs

--- src/main/java/project/flowchat/backend/Controller/SystemController.java
+++ src/main/java/project/flowchat/backend/Controller/SystemController.java
@@ -1,23 +1,29 @@
 package project.flowchat.backend.Controller;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import project.flowchat.backend.Model.SystemModel;
 import project.flowchat.backend.Service.SystemService;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@CrossOrigin
 @RequestMapping("api/System")
 public class SystemController {
 
     @Autowired
     private final SystemService systemService;
 
-    @GetMapping("getWebName")
-    private String getWebName() {
-        return systemService.getWebName();
+    @GetMapping("getAllInfo")
+    private List<SystemModel> getAllInfo() {
+        return systemService.getAllInfo();
+    }
+
+    @GetMapping("getInfoByVersion")
+    private List<SystemModel> getInfoByVersion(@RequestParam String version) {
+        return systemService.getInfoByVersion(version);
     }
 }
