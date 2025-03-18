5e340ee8a3322ed6059634c5b760457436ca887b
royng163 <royng163@gmail.com>
2025-03-18 22:26:33 +0800

    Config middleware for deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,28 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === '/login' || path === '/register' || path === '/forgot-password';
-  
+  const isPublicPath = path === "/login" || path === "/register" || path === "/forgot-password";
+
   // Get token from cookies
-  const token = request.cookies.get('user')?.value || '';
-  
+  const token = request.cookies.get("user")?.value || "";
+
   // Redirect logic
-  console.log('path', path);
-    console.log('isPublicPath', isPublicPath);
+  console.log("path", path);
+  console.log("isPublicPath", isPublicPath);
   if (isPublicPath && token) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
-  
-  if (!isPublicPath && !token && path !== '/') {
-    return NextResponse.redirect(new URL('/login', request.url));
+
+  if (!isPublicPath && !token && path !== "/") {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
-  
+
   return NextResponse.next();
 }
 
 // Match all paths except static files and api routes
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.swa).*)"],
 };
