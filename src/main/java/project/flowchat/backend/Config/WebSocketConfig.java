d21af9f25bf4f29b8af0f6fe502938f9f60170a9
chris <chrischan52504@gmail.com>
2025-03-29 03:16:53 +0800

    Add code to only allow WebSocket connection if JWT is valid
    Only need to add JWT in the header when starting connection

--- src/main/java/project/flowchat/backend/Config/WebSocketConfig.java
+++ src/main/java/project/flowchat/backend/Config/WebSocketConfig.java
@@ -1,14 +1,22 @@
 package project.flowchat.backend.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+
+    @Autowired
+    private final JwtChannelInterceptor jwtChannelInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -22,4 +30,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
                 .setAllowedOrigins("http://localhost:3000", "https://kind-wave-0b69df000.6.azurestaticapps.net")
                 .withSockJS();
     }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtChannelInterceptor);
+    }    
 }
